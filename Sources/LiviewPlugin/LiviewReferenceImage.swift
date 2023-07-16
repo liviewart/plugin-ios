1b96401979e71d20338b326ddba86320bb1aa7a2
j-jirgl <jakub.jirgl@blendlens.com>
2023-07-16 10:39:27 +0200

    Add body of init

--- Sources/LiviewPlugin/LiviewReferenceImage.swift
+++ Sources/LiviewPlugin/LiviewReferenceImage.swift
@@ -3,8 +3,13 @@ import Foundation
 
 
 public struct LiviewReferenceImage: Identifiable {
-    public init()
     public let id: UUID
     public let image: CGImage
     public let width: CGFloat
+
+    public init(id: UUID, image: CGImage, width: CGFloat) {
+        self.id = id
+        self.image = image
+        self.width = width
+    }
 }
--- Sources/LiviewPlugin/StorageInterface.swift
+++ Sources/LiviewPlugin/StorageInterface.swift
@@ -1,3 +1,4 @@
 public struct StorageInterface {
-    public init()
+    public init() {
+    }
 }
