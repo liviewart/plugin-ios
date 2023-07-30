c33b24c38b537f937b7da56af05758d12ca06800
j-jirgl <jakub.jirgl@blendlens.com>
2023-07-30 08:25:38 +0200

    Make constructor public

--- Sources/LiviewPlugin/StorageInterface.swift
+++ Sources/LiviewPlugin/StorageInterface.swift
@@ -4,6 +4,11 @@ import Foundation
 public struct LiviewData {
     let image: Data?
     let metadata: Data?
+
+    public init(image: Data?, metadata: Data?) {
+        self.image = image
+        self.metadata = metadata
+    }
 }
 
 public protocol StorageInterface {
