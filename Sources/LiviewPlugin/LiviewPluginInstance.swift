6dd3f6e6b6e5aff44f22765e6ed6e8764175305d
j-jirgl <jakub.jirgl@blendlens.com>
2023-08-06 21:12:05 +0200

    Fix return types

--- Sources/LiviewPlugin/LiviewPluginInstance.swift
+++ Sources/LiviewPlugin/LiviewPluginInstance.swift
@@ -5,7 +5,7 @@ import RealityKit
 public protocol LiviewPluginInstance {
     init(storage: StorageInterface)
     func getName() -> String
-    func getModel(referenceImageUUID: UUID) async -> (RealityFoundation.Entity?) -> Void
-    func getReferenceImages() async -> ([LiviewReferenceImage]) -> Void
-    func tryToRegister(_ data: String) async -> (LiviewItem?) -> Void
+    func getModel(referenceImageUUID: UUID) async -> RealityFoundation.Entity?
+    func getReferenceImages() async -> [LiviewReferenceImage]
+    func tryToRegister(_ data: String) async -> LiviewItem?
 }
