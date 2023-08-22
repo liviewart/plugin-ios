d8030911135c628551e122701b708145ce3a81f5
j-jirgl <jakub.jirgl@blendlens.com>
2023-08-22 15:45:04 +0200

    Add public constructors

--- Sources/LiviewPlugin/Models/LiviewPlane.swift
+++ Sources/LiviewPlugin/Models/LiviewPlane.swift
@@ -9,4 +9,9 @@ public enum PlaneType {
 public struct LiviewPlane {
     public let type: PlaneType
     public let scene: LiviewScene
+
+    public init(type: PlaneType, scene: LiviewScene) {
+        self.type = type
+        self.scene = scene
+    }
 }
--- Sources/LiviewPlugin/Models/LiviewScene.swift
+++ Sources/LiviewPlugin/Models/LiviewScene.swift
@@ -4,4 +4,9 @@ import Foundation
 public struct LiviewScene: Identifiable {
     public let id: UUID
     public let name: String
+
+    public init(id: UUID, name: String) {
+        self.id = id
+        self.name = name
+    }
 }
