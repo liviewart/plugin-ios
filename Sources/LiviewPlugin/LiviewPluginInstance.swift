54f20853fc2942c6f76ea213e04c11097a3890a2
jirgl <jirgl.jakub@gmail.com>
2023-10-23 13:34:29 +0200

    Update protocol

--- Sources/LiviewPlugin/LiviewPluginInstance.swift
+++ Sources/LiviewPlugin/LiviewPluginInstance.swift
@@ -1,7 +1,7 @@
 import Foundation
 import RealityKit
 
-
 public protocol LiviewPluginInstance {
+    var pluginInfo: PluginInfo { get }
     func getScene(sceneUUID: UUID, width: Float, height: Float) async -> RealityFoundation.Entity?
 }
---
+++ Sources/LiviewPlugin/PluginInfo.swift
@@ -0,0 +1,5 @@
+import Foundation
+
+public struct PluginInfo {
+    public let uuid: UUID
+}
