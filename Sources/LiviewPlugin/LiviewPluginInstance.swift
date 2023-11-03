8ead471cef9ccb92b3de122d67ae9da60990e526
jirgl <jirgl.jakub@gmail.com>
2023-11-03 13:59:04 +0100

    Update protocol

--- Sources/LiviewPlugin/LiviewPluginInstance.swift
+++ Sources/LiviewPlugin/LiviewPluginInstance.swift
@@ -1,7 +1,9 @@
 import Foundation
 import RealityKit
 
+@available(macOS 10.15, *)
 public protocol LiviewPluginInstance {
     var pluginInfo: PluginInfo { get }
     func getScene(sceneUUID: UUID, width: Float, height: Float) async -> RealityFoundation.Entity?
+    func getScene(sceneUUID: UUID, width: Float, height: Float, onCompletion: (RealityFoundation.Entity?) -> Void)
 }
