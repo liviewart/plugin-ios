9fd638a4ab71db90b690000a42d55cd808046b00
jirgl <jirgl.jakub@gmail.com>
2023-11-24 09:45:53 +0100

    Polish protocol

--- Sources/LiviewPlugin/LiviewPluginInstance.swift
+++ Sources/LiviewPlugin/LiviewPluginInstance.swift
@@ -4,5 +4,5 @@ import RealityKit
 @available(macOS 10.15, *)
 public protocol LiviewPluginInstance {
     var pluginInfo: PluginInfo { get }
-    func getScene(sceneUUID: UUID, width: Float, height: Float, onCompletion: (RealityFoundation.Entity) -> Void)
+    func getScene(sceneUUID: UUID, width: Float, height: Float, onCompletion: @escaping (RealityFoundation.Entity) -> Void)
 }
